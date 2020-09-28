1638110cc691936d8449d8853747802b5a17df8e
Jordi <jordimesas54gmail.com>
2020-09-28 19:51:16 +0200

    update ebooks to dinamic column right div

--- view/ebooks.php
+++ view/ebooks.php
@@ -1,38 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Nuestro proyecto Re-Read</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<!--Estilos enlazados-->
-		<link rel="stylesheet" href="../css/estilo.css" />
-	</head>
 
-	<body>
-		<div class="logo">Re-Read</div>
+<head>
+    <title>Nuestro proyecto Re-Read</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--Estilos enlazados-->
+    <link rel="stylesheet" href="../css/estilo.css" />
+</head>
 
-		<div class="header">
-			<h1>Re-Read</h1>
-			<p>
-				En Re-Read podrás encontrar libros de segunda mano en perfecto estado.
-				También vender los tuyos. Porque siempre hay libros leídos y libros por
-				leer. Por eso Re-compramos y Re-vendemos para que nunca te quedes sin
-				ninguno de los dos.
-			</p>
-		</div>
+<body>
+    <div class="logo">Re-Read</div>
 
-		<div class="row">
-			<div class="column left">
-				<div class="topnav">
-					<a href="../index.php">Re-Read</a>
-					<a href="./libros.php">Libros</a>
-					<a href="./ebooks.php">eBook</a>
-				</div>
+    <div class="header">
+        <h1>Re-Read</h1>
+        <p>
+            En Re-Read podrás encontrar libros de segunda mano en perfecto estado.
+            También vender los tuyos. Porque siempre hay libros leídos y libros por
+            leer. Por eso Re-compramos y Re-vendemos para que nunca te quedes sin
+            ninguno de los dos.
+        </p>
+    </div>
 
-				<h3>Toda la actualidad en eBook</h3>
+    <div class="row">
+        <div class="column left">
+            <div class="topnav">
+                <a href="../index.php">Re-Read</a>
+                <a href="./libros.php">Libros</a>
+                <a href="./ebooks.php">eBook</a>
+            </div>
 
-				<!--eBooks con descripción-->
-				<!--<div class="ebook">
+            <h3>Toda la actualidad en eBook</h3>
+
+            <!--eBooks con descripción-->
+            <!--<div class="ebook">
 					<a
 						href="https://play.google.com/store/books/details/Cube_Kid_El_gatito_que_se_perdi%C3%B3_en_el_Inframundo?id=l7jbDwAAQBAJ&gl=ES"
 					>
@@ -41,7 +42,7 @@
 					>
 				</div>-->
 
-				<?php
+            <?php
 				//1. Conexión con la base de datos
 				include '../services/connection.php';
 
@@ -69,16 +70,31 @@
 					echo "0 resultados";
 				}
 				?>
-				
-			</div>
 
-			<div class="column right">
-				<h2>Top ventas</h2>
-				<p>Cien años de soledad.</p>
-				<p>Crónica de una muerte anunciada.</p>
-				<p>El otoño del patriarca.</p>
-				<p>El general en su laberinto.</p>
-			</div>
-		</div>
-	</body>
+        </div>
+
+        <div class="column right">
+            <h2>Top ventas</h2>
+
+            <?php
+			
+			$result = mysqli_query($conn, "SELECT Books.Title FROM books WHERE Top = '1'");
+
+				if(!empty($result) && mysqli_num_rows($result)>0){
+
+						while($row = mysqli_fetch_array($result)){							
+							echo"<p>";												
+							echo $row['Title'];							
+							echo "</p>";						
+						}
+						
+				}else{
+						echo "0 resultados";
+					}
+			 ?>
+
+        </div>
+    </div>
+</body>
+
 </html>
